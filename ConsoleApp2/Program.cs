dd19301a25123a80921ca236bedea7af94dce5fa
William <williamwr@gmail.com>
2020-05-10 13:02:06 -0400

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.IO;
+
+using System.Security.Cryptography;
+
+namespace ExampleTreeHash
+{
+    class Program
+    {
+        static int ONE_MB = 1024 * 1024;
+
+        /**
+        * Compute the Hex representation of the SHA-256 tree hash for the
+        * specified file
+        * 
+        * @param args
+        *            args[0]: a file to compute a SHA-256 tree hash for
+        */
+        public static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Missing required filename argument");
+                Environment.Exit(-1);
+            }
+            FileStream inputFile = File.Open(args[0], FileMode.Open, FileAccess.Read);
+            try
+            {
+                byte[] treeHash = ComputeSHA256TreeHash(inputFile);
+                Console.WriteLine("SHA-256 Tree Hash = {0}", BitConverter.ToString(treeHash).Replace("-", "").ToLower());
+                Console.ReadLine();
+                Environment.Exit(-1);
+            }
+            catch (IOException ioe)
+            {
+                Console.WriteLine("Exception when reading from file {0}: {1}",
+                    inputFile, ioe.Message);
+                Console.ReadLine();
+                Environment.Exit(-1);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Cannot locate MessageDigest algorithm for SHA-256: {0}",
+                    e.Message);
+                Console.WriteLine(e.GetType());
+                Console.ReadLine();
+                Environment.Exit(-1);
+            }
+            Console.ReadLine();
+        }
+
+
+        /**
+         * Computes the SHA-256 tree hash for the given file
+         * 
+         * @param inputFile
+         *            A file to compute the SHA-256 tree hash for
+         * @return a byte[] containing the SHA-256 tree hash
+         */
+        public static byte[] ComputeSHA256TreeHash(FileStream inputFile)
+        {
+            byte[][] chunkSHA256Hashes = GetChunkSHA256Hashes(inputFile);
+            return ComputeSHA256TreeHash(chunkSHA256Hashes);
+        }
+
+
+        /**
+         * Computes a SHA256 checksum for each 1 MB chunk of the input file. This
+         * includes the checksum for the last chunk even if it is smaller than 1 MB.
+         * 
+         * @param file
+         *            A file to compute checksums on
+         * @return a byte[][] containing the checksums of each 1MB chunk
+         */
+        public static byte[][] GetChunkSHA256Hashes(FileStream file)
+        {
+            long numChunks = file.Length / ONE_MB;
+            if (file.Length % ONE_MB > 0)
+            {
+                numChunks++;
+            }
+
+            if (numChunks == 0)
+            {
+                return new byte[][] { CalculateSHA256Hash(null, 0) };
+            }
+            byte[][] chunkSHA256Hashes = new byte[(int)numChunks][];
+
+            try
+            {
+                byte[] buff = new byte[ONE_MB];
+
+                int bytesRead;
+                int idx = 0;
+
+                while ((bytesRead = file.Read(buff, 0, ONE_MB)) > 0)
+                {
+                    chunkSHA256Hashes[idx++] = CalculateSHA256Hash(buff, bytesRead);
+                }
+                return chunkSHA256Hashes;
+            }
+            finally
+            {
+                if (file != null)
+                {
+                    try
+                    {
+                        file.Close();
+                    }
+                    catch (IOException ioe)
+                    {
+                        throw ioe;
+                    }
+                }
+            }
+
+        }
+
+        /**
+         * Computes the SHA-256 tree hash for the passed array of 1MB chunk
+         * checksums.
+         * 
+         * This method uses a pair of arrays to iteratively compute the tree hash
+         * level by level. Each iteration takes two adjacent elements from the
+         * previous level source array, computes the SHA-256 hash on their
+         * concatenated value and places the result in the next level's destination
+         * array. At the end of an iteration, the destination array becomes the
+         * source array for the next level.
+         * 
+         * @param chunkSHA256Hashes
+         *            An array of SHA-256 checksums
+         * @return A byte[] containing the SHA-256 tree hash for the input chunks
+         */
+        public static byte[] ComputeSHA256TreeHash(byte[][] chunkSHA256Hashes)
+        {
+            byte[][] prevLvlHashes = chunkSHA256Hashes;
+            while (prevLvlHashes.GetLength(0) > 1)
+            {
+
+                int len = prevLvlHashes.GetLength(0) / 2;
+                if (prevLvlHashes.GetLength(0) % 2 != 0)
+                {
+                    len++;
+                }
+
+                byte[][] currLvlHashes = new byte[len][];
+
+                int j = 0;
+                for (int i = 0; i < prevLvlHashes.GetLength(0); i = i + 2, j++)
+                {
+
+                    // If there are at least two elements remaining
+                    if (prevLvlHashes.GetLength(0) - i > 1)
+                    {
+
+                        // Calculate a digest of the concatenated nodes
+                        byte[] firstPart = prevLvlHashes[i];
+                        byte[] secondPart = prevLvlHashes[i + 1];
+                        byte[] concatenation = new byte[firstPart.Length + secondPart.Length];
+                        System.Buffer.BlockCopy(firstPart, 0, concatenation, 0, firstPart.Length);
+                        System.Buffer.BlockCopy(secondPart, 0, concatenation, firstPart.Length, secondPart.Length);
+
+                        currLvlHashes[j] = CalculateSHA256Hash(concatenation, concatenation.Length);
+
+                    }
+                    else
+                    { // Take care of remaining odd chunk
+                        currLvlHashes[j] = prevLvlHashes[i];
+                    }
+                }
+
+                prevLvlHashes = currLvlHashes;
+            }
+
+            return prevLvlHashes[0];
+        }
+
+        public static byte[] CalculateSHA256Hash(byte[] inputBytes, int count)
+        {
+            SHA256 sha256 = System.Security.Cryptography.SHA256.Create();
+            byte[] hash = sha256.ComputeHash(inputBytes, 0, count);
+            return hash;
+        }
+    }
+}
